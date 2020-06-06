dec0ea23d6e42b21f6b7bf93b85a8a5deab54a14
nekkantirohit <58840432+nekkantirohit@users.noreply.github.com>
2020-06-06 08:21:25 +0530

    Add files via upload

--- sample project/p1.js
+++ sample project/p1.js
@@ -1,53 +1,88 @@
-function validate(){
-    if(document.getElementById("name").value==""){
-    window.alert( "Please provide your Name!" );
-    document.getElementById("name").focus;
+function validate() {
+    var result = "";
+    result += validateName();
+    result += validateId();
+    result += validatedob();
+    result += validated();
+    result += validatemb();
+    result += validateEmail();
+    result += validatePassword();
+    result += validateTerms();
+  
+    if (result == "") return true;
+    else alert("Validation Result:\n\n" + result);
+  
     return false;
+  }
+  
+  function validateName() {
+    var name = document.getElementById("us").value;
+    if (name.length <= 3) return "Name should be at least three characters.\n";
+    else return "";
+  }
+  function validateId() {
+    var i = document.getElementById("idd").value;
+    var i1 = /^[2][2][1][7][1][0][3][0-9]{2}[0-9]{3}$/;
+    if (i1.test(i)) {
+      return "";
+    } else return "Enter correct ID in GITAM format.\n";
+  }
+  function validatedob() {
+    var d = document.getElementById("dt").value;
+    var reg = /^(0[1-9]|[1-2][0-9]|3[0-1])[/](0[1-9]|1[1-2])[/]([19|20][0-9]{3})$/;
+    if (reg.test(d)) {
+      return "";
+    } else return "Enter Valid DOB in dd/mm/yyyy Format.\n";
+  }
+  function validatemb() {
+    var mb = document.getElementById("ph").value;
+    var g = /^[6-9][0-9]{9}$/;
+    if (g.test(mb)) {
+      return "";
+    } else {
+      return "Enter Valid Mobile number.\n";
     }
-    if( document.getElementById("rollno").value == "" ||
-           isNaN( document.getElementById("rollno").value) ||
-           document.getElementById("rollno").value.length != 10 )
-   {
-     window.alert( "Please provide a rollno No in the format 123." );
-     document.getElementById("rollno").focus ;
-     return false;
-   }
- var email=document.getElementById("emailId").value;
- atpos=indexOf('@');
- dotpos=indexOf('.')
- if(email==""||atpos<1||dotpos-atpos<2){
-     window.alert("enter correct email.id");
-     document.getElementById("emailId").focus;
-     return false;
- }
- if(document.getElementById("personal address").value==""){
-     window.alert("enter correct address");
-     document.getElementById("personal address").focus;
-     return false;
- }
- if(document.getElementById("department").value==-1){
-     window.alert("please choose your department");
-     return false;
- }
- if(document.getElementById("joining year").value==-1){
-    window.alert("please choose your joining year");
-    return false;
- }
- if(document.getElementById("passing year").value==-1){
-    window.alert("please choose your passing year");
-    return false;
- }
- if(document.getElementById("dob").value==""){
-    window.alert("enter your dob");
-    document.getElementById("dob").focus;
-    return false;
-}
-if( document.getElementById("mobileno").value == "" ||
-isNaN( document.getElementById("mobileno").value) ||
-document.getElementById("mobileno").value.length != 10 )
-{
-window.alert( "Please provide a Mobile No in the format 123." );
-document.getElementById("mobileno").focus ;
-return false;
-}
-}
+  }
+  
+  function validatePassword() {
+    var password = document.getElementById("ps").value;
+    var retype = document.getElementById("rps").value;
+    var pss = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,12}$/;
+    if (pss.test(password)) {
+      return "";
+    }
+    if (password != retype) {
+      return "Passwords do not match.\n";
+    } else
+      return "Password should be at least 6 characters and should have atleast 1Uppercase letter, Special chars and digits\n";
+  }
+  
+  function validateEmail() {
+    var email = document.getElementById("em").value;
+    var retype = document.getElementById("rem").value;
+    var emm = /^[a-zA-z0-9._-]{6,12}[@][a-z]{3,15}[.][a-z]{2,6}$/;
+  
+    if (emm.test(email)) {
+      return "";
+    }
+    if (email != retype) {
+      return "Email addresses do not match.\n";
+    } else return "Enter Correct Email.\n";
+  }
+  function validateTerms() {
+    var terms = document.getElementById("cb").value;
+    if (!terms.checked)
+      return "Please accept the Terms of Service and Privacy Policy.\n";
+    else return "";
+  }
+  
+  function validated() {
+    po = document.getElementById("pd").value;
+    jo = document.getElementById("jd").value;
+    if (po - jo <= 4) {
+      return "";
+    } else return "Your Passes out .\n";
+  }
+  function myFunction() {
+    document.getElementById("f1").reset();
+  }
